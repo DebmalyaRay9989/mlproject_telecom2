241e481a398faa92516594de1c6432b565c4a72a
Debmalya Ray <66347154+DebmalyaRay9989@users.noreply.github.com>
2023-08-08 19:36:23 +0530

    Update app.py
    .

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
+
 TEST_DB = "database.db"
 
 application = Flask(__name__)
